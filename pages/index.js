3912df3d3e7a696edf320dc2e242da24f983e1ca
yamadamasashi <shisama94@gmail.com>
2020-06-23 23:11:24 +0900

    つぎはスタイリングに関するtipsから

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home () {
         <p><a href="https://github.com/masashi49">js太郎のGithub</a>
         </p>
         <Link href="/posts/first-post"><a>first-page</a></Link>
-        次は<a href="https://qiita.com/thesugar/items/01896c1faa8241e6b1bc">ここから</a>スタートです
+        次は<a href="https://qiita.com/thesugar/items/01896c1faa8241e6b1bc#%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B-tips">ここから</a>スタートです
       </section>
     </Layout>
   )
