7fcd715cd8c75ec996edc038eb4e5a334c2cbd53
yamada.masashi <yamada.masashi@a-tm.co.jp>
2020-07-11 00:55:47 +0900

    tes

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,6 @@ export default function Home ( { allPostsData } ) {
         <p>こんにちはjs太郎です</p>
         <p><a href="https://github.com/masashi49">js太郎のGithub</a>
         </p>
-        <Link href="/posts/first-post"><a>first-page</a></Link>
         次は<a href="https://qiita.com/thesugar/items/01896c1faa8241e6b1bc#%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B-tips">ここから</a>スタートです
       </section>
       <section className={ `${ utilStyles.headingMd } ${ utilStyles.padding1px }` }>
@@ -37,7 +36,7 @@ export default function Home ( { allPostsData } ) {
             <li className={ utilStyles.listItem } key={ id }>
               { title }
               <br />
-              { id }
+              <Link href={`/posts/${id}`}><a>{ id }</a></Link>
               <br />
               { date }
             </li>
