19f2199341d4f9997d8b674c2dc41c6c88bbb51b
yamadamasashi <shisama94@gmail.com>
2021-09-29 22:47:49 +0900

    タイトル消した

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'ジェイエス太郎です'
-export const siteTitle = '私のテストサイトaaaaaaaaaaa'
+export const siteTitle = 'こんにいとぁ'
 
 
 
